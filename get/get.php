3e2720ffc8bb041e27db09f3d1b4cbf3cf848f13
Milan9135 <Milpower0@gmail.com>
2023-05-08 18:23:33 +0200

    e

---
+++ get/get.php
@@ -0,0 +1,15 @@
+<?php
+
+$naam = $_GET["name"];
+$achternaam = $_GET["last-name"];
+$email = $_GET["email"];
+$age = $_GET["age"];
+$adress = $_GET["adress"];
+
+echo("$naam ");
+echo("$achternaam ");
+echo("$email ");
+echo("$age ");
+echo("$adress ");
+
+?>
---
+++ post/post.php
@@ -0,0 +1,10 @@
+<?php
+
+$naam = $_POST["name"];
+$leeftijd = $_POST["age"];
+
+echo("$naam ");
+echo("$leeftijd ");
+
+
+?>
